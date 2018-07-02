00ea59d83eafd7030fbf876e1463b499e8e510c2
Denis Li <denisli268@gmail.com>
2018-07-02 19:19:08 -0400

    Code refactoring. Moved the <script> tag enclosed code to its own file.

---
+++ public/js/main.js
@@ -0,0 +1,70 @@
+// ready() checks that document.body has loaded. This is required by the Quill library
+$(document).ready(function() {
+  // Get the contents for today's daily
+  var getDailyFromDate = function(date) {
+    $.get({
+      url: "/daily/" + date.getFullYear() + "/" + date.getMonth() + "/" + date.getDate(),
+      datatype: "json",
+      success: function(data, err) {
+        console.log("Successfully got daily for", date);
+        if (data) {
+          editor.setContents(JSON.parse(data.content));
+        }
+        else {
+          editor.setContents(""); // empty contents
+        }
+        change = new Delta(); // change is initialized below
+      },
+      error: function(err) {
+        console.log("Failed to get daily for", date);
+        console.log(err);
+      }
+    });
+  };
+
+  // Load the date picker
+  var picker = new Pikaday({
+    field: document.getElementById('datepicker'),
+    onSelect: getDailyFromDate,
+    firstDay: 1,
+    maxDate: new Date(2020, 12, 31),
+    yearRange: [2000,2020],
+  });
+  picker.setDate(new Date());
+
+  // Load the editor
+  var Delta = Quill.import('delta');
+  var editor = new Quill('#quill-editor', {
+    placeholder: 'Write about your day',
+    theme: 'snow',
+  });
+
+  // Store accumulated changes
+  var change = new Delta();
+  editor.on('text-change', function(delta) {
+    change = change.compose(delta);
+  });
+
+  // Auto-save changes to the daily
+  setInterval(function() {
+    if (change.length() > 0) {
+      var date = picker.getDate();
+      var content = JSON.stringify(editor.getContents());
+      $.ajax({
+        method: 'PUT',
+        url: "/daily/" + date.getFullYear() + "/" + date.getMonth() + "/" + date.getDate(),
+        contentType: 'application/json',
+        data: JSON.stringify({ content: content }),
+        datatype: "json",
+        success: function(data, err) {
+          console.log("Successfully saved daily");
+          change = new Delta();
+        },
+        error: function(err) {
+          console.log("Failed to save daily");
+          console.log(err);
+        }
+      });
+    }
+  }, 5000); // every 5 seconds
+});
